88d767522c3f3b29a7c06a7b9758cdbe205973a0
Luke Gaudreau <luke@infosoph.org>
2016-09-14 16:02:33 -0400

    Initial creation of enhance script

---
+++ enhance.sh
@@ -0,0 +1,40 @@
+#/bin/sh
+
+OAIFILES="../oai_harvester/harvested/*.xml"
+OUTDIR="enhanced"
+HATHIFILES="https://www.hathitrust.org/filebrowser/download/159922"
+
+# Get HathiTrust Record Numbers from OAI files
+echo "Extracting record numbers"
+for f in $OAIFILES
+do
+  name=$(basename $f .xml)
+  saxon -s:$f -xsl:extractIdentifiers.xsl -o:$OUTDIR/$name.txt
+done
+
+# Concatenate all IDs to single file and sort them
+echo "Sorting all OAI IDs"
+cat $OUTDIR/*.txt | sort > $OUTDIR/all-OAI-ids
+
+# Get the latest Hathifiles
+echo "Fetching the latest Hathifiles"
+curl $HATHIFILES -o $OUTDIR/hathi_full.txt.gz
+
+echo "Uncompressing Hathifiles"
+gunzip $OUTDIR/hathi_full.txt.gz
+
+# Create lookup from sorted IDs
+echo "Creating lookup from HathiFiles"
+java -jar parseHathiFiles.jar $OUTDIR/hathi_full.txt $OUTDIR/IdToTitle $OUTDIR/all-OAI-ids
+
+# Sort the results
+echo "Sorting lookup"
+sort $OUTDIR/IdToTitle > $OUTDIR/IdToTitleSorted
+
+# Create title lookup file
+echo "Creating title lookup file"
+perl createTitleLookup.pl $OUTDIR/IdToTitleSorted
+mv titleMerge.xml $OUTDIR/
+
+# Where does OAI.xml come from?
+saxon -s:$OUTDIR/OAI.xml -xsl:mergeHathiFiles.xsl -o:$OUTDIR/merged.xml
